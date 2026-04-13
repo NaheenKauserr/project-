446b14330548a638feca51e555c54ad466f1112f
Naheen Kauser <naheenkauser113@gmail.com>
2026-04-13 21:12:09 +0530

    Delete module 1 directory

--- module 1/data_quality.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def data_quality_section(df):
-    st.title("🔍 Data Quality Report")
-    st.write("Missing values:", df.isnull().sum())
--- module 1/dataset_profiling.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-def dataset_profiling_section(df):
-    st.title("📊 Dataset Profiling")
-    st.dataframe(df.describe())
